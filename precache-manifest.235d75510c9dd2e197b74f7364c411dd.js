ab8a78b2731d6fe6ab1d03d8a67371db7847b7b1
jacobwicks <jacob.m.wicks@gmail.com>
2020-01-23 14:28:00 -0800

    Updates

---
+++ precache-manifest.235d75510c9dd2e197b74f7364c411dd.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1cc347f5aaca2d1f383af940a8d8c597",
+    "url": "/dndlogic/index.html"
+  },
+  {
+    "revision": "40ae8339949f81500f61",
+    "url": "/dndlogic/static/css/main.83bd8ac9.chunk.css"
+  },
+  {
+    "revision": "bdb0d4a5b0490e584bb5",
+    "url": "/dndlogic/static/js/2.315f5c3b.chunk.js"
+  },
+  {
+    "revision": "7641e197ad52ce70804a34daaad296ee",
+    "url": "/dndlogic/static/js/2.315f5c3b.chunk.js.LICENSE"
+  },
+  {
+    "revision": "40ae8339949f81500f61",
+    "url": "/dndlogic/static/js/main.5b255c06.chunk.js"
+  },
+  {
+    "revision": "34b08128a9e307b15d06",
+    "url": "/dndlogic/static/js/runtime-main.ad7ad7fd.js"
+  }
+]);
